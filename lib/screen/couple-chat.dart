8916bcb5facbf3d62a76883b2aa4e0ffcdd3ab99
Tran Ha Nam <tranhanam1999hn@gmail.com>
2022-04-03 14:59:51 +0700

    feat: add singleton user info

--- lib/screen/couple-chat.dart
+++ lib/screen/couple-chat.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screen/chat/list-view-chat.dart';
+import 'package:flutter_application_1/store/user.dart';
 
 class CoupleChat extends StatelessWidget {
   const CoupleChat({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var user = UserInfo.getInstance();
+    print(user?.userId);
+
     return Scaffold(
       body: Center(
         child: Column(
---
+++ lib/store/user.dart
@@ -0,0 +1,20 @@
+class UserInfo {
+  // variables
+  late String? username = "";
+  late String? password = "";
+  late String? userId = "";
+  late String? token = "";
+
+  UserInfo({this.username, this.password, this.userId, this.token});
+
+  static UserInfo? _instance;
+
+  static UserInfo? getInstance({username, password, userId, token}) {
+    if (_instance == null) {
+      _instance =
+          UserInfo(username: username, password: password, userId: userId, token: token);
+      return _instance;
+    }
+    return _instance;
+  }
+}
