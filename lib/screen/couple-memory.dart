537cc7ac2f9528fbc149795be9141a0a1627f8c9
Tran Ha Nam <tranhanam1999hn@gmail.com>
2022-04-21 23:18:13 +0700

    imp: memory page

--- lib/screen/couple-memory.dart
+++ lib/screen/couple-memory.dart
@@ -17,12 +17,34 @@ class CoupleMemory extends StatefulWidget {
 
 class _MyCoupleMemoryScreenState extends State<CoupleMemory> {
   var client = new http.Client();
+  String selectedUsername = "Chưa có";
+  late Future<List<String>> usernames;
   var partnerId = PartnerInfo.getInstance()?.userId;
 
   @override
   void initState() {
     super.initState();
+
+    usernames = getAllUsers();
     findPartner();
+  }
+
+  Future<List<String>> getAllUsers() async {
+    final url = Uri.parse(API_DOMAIN + "/user/list");
+    var getAllUsersResp = await client.get(url);
+
+    if (getAllUsersResp.statusCode == 200) {
+      var decodedBody = jsonDecode(getAllUsersResp.body);
+      List<dynamic> list = decodedBody['data'];
+
+      List<String> tempUsernames =
+          List<String>.from(list.map((model) => model['username']));
+
+      tempUsernames.add("Chưa có");
+      return tempUsernames;
+    } else {
+      throw Exception('Failed to load album');
+    }
   }
 
   void findPartner() async {
@@ -48,9 +70,29 @@ class _MyCoupleMemoryScreenState extends State<CoupleMemory> {
         token: "");
   }
 
+
+  void validateSelectedPartner(context) async {
+    var user = UserInfo.getInstance();
+    var userId = user?.userId;
+
+    final url = Uri.parse(API_DOMAIN + "/user/verify-partner" + "?userId=$userId&?partnerUsername=$selectedUsername");
+
+    Map<String, String> headers = {"Content-type": "application/json"};
+
+    var getPartnerResp = await client.get(url, headers: headers);
+    String body = getPartnerResp.body;
+
+    var loggedUser = jsonDecode(body);
+
+    if (getPartnerResp.statusCode == 200) {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (PartnerInfo.getInstance()?.userId == "") {
+    if (PartnerInfo.getInstance()?.userId == "" ||
+        PartnerInfo.getInstance()?.userId == null) {
       return Scaffold(
           body: Center(
               child: Column(
@@ -58,11 +100,42 @@ class _MyCoupleMemoryScreenState extends State<CoupleMemory> {
         children: [
           AlertDialog(
             title: Text("Alert Dialog Box"),
-            content: Text("You have raised a Alert Dialog Box"),
+            content: FutureBuilder<List<String>>(
+              future: usernames,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var user = UserInfo.getInstance();
+                  var userId = user?.userId;
+
+                  return Expanded(
+                      child: DropdownButton<String>(
+                    value: selectedUsername,
+                    items: snapshot.data?.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedUsername = value!;
+                      });
+                    },
+                  ));
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),
             actions: <Widget>[
               FlatButton(
                 onPressed: () {
-                  Navigator.of(context).pop();
+                  if (selectedUsername != "Chưa có") {
+                    validateSelectedPartner(context);
+                  }
                 },
                 child: Text("okay"),
               ),
@@ -83,14 +156,14 @@ class _MyCoupleMemoryScreenState extends State<CoupleMemory> {
                     'Đã ở bên nhau được 100 ngày',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   )),
-              AlertDialog(
-                title: Text("Success"),
-                actions: [
-                  CupertinoDialogAction(onPressed: () {}, child: Text("Back")),
-                  CupertinoDialogAction(onPressed: () {}, child: Text("Next")),
-                ],
-                content: Text("Saved successfully"),
-              ),
+              // AlertDialog(
+              //   title: Text("Success"),
+              //   actions: [
+              //     CupertinoDialogAction(onPressed: () {}, child: Text("Back")),
+              //     CupertinoDialogAction(onPressed: () {}, child: Text("Next")),
+              //   ],
+              //   content: Text("Saved successfully"),
+              // ),
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
